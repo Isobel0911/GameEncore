ec1b6b8d52dc8385592f741790e80a60b85d0fa4
Isobel0911 <jdeng65@gatech.edu>
2023-11-17 11:22:06 -0500

    Fix Alert bar bug

--- Assets/Scripts/AlertController.cs
+++ Assets/Scripts/AlertController.cs
@@ -6,11 +6,6 @@ public class AlertController : MonoBehaviour
 {
     public ProgressBar pb;
     public int alert = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
