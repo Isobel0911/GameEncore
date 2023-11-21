aa0697b537a190dd35fe8fe303a3170b213d5064
Isobel0911 <jdeng65@gatech.edu>
2023-11-21 00:21:26 -0500

    Add Puzzle to computer Interaction
    *If solved, no longer interactable
    * Dialogue prompted

--- Assets/PuzzleInteract.cs
+++ Assets/PuzzleInteract.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PuzzleInteract : MonoBehaviour 
+public class PuzzleInteract : MonoBehaviour, IInteractable
 {
+    public Message[] messages;
+    public Actor[] actors;
     // Start is called before the first frame update
     [SerializeField] private string _prompt;
 
     public string InteractionPrompt => _prompt;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public static bool hasSolved = false;
+    public bool Interact(Interactor interactor) {
+        // Open puzzle scene
+        if (!hasSolved) {
+            Debug.Log("Open Scene request");
+            Cursor.visible = true;
+            SceneManager.LoadScene("SlidingTilePuzzle", LoadSceneMode.Additive);
+            return true;
+        } else {
+            // show dialogue says you have decoded the documents.
+            FindObjectOfType<DialogueManager>().OpenDialogue(messages, actors);
+            return true;
+        }
     }
 }
